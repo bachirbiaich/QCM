597703a36a1fb8a4061026258a07ff22d1387206
Bachir <bachir.biaich@gmail.com>
2017-03-17 16:03:11 +0100

    Vue pour la création des QCM

--- src/QcmBundle/Controller/CreateController.php
+++ src/QcmBundle/Controller/CreateController.php
@@ -3,8 +3,12 @@
 namespace QcmBundle\Controller;
 
 
+use QcmBundle\Entity\Qcm;
+use QcmBundle\Entity\Question;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CreateController extends Controller
 {
@@ -12,9 +16,16 @@ class CreateController extends Controller
     /**
      * @Route("/",name="qcmCreate_index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('QcmBundle:Create:index.html.twig');
+        if($request->isMethod('GET'))
+        {
+            return $this->render('QcmBundle:Create:index.html.twig');
+        }
+        else if($request->isMethod('POST'))
+        {
+            return new Response("POST");
+        }
     }
 
 }
